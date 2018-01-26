1328c68527343f4c852acec4ab380cb4c7a70e0e
badongsch <badongsch@gmail.com>
2018-01-26 17:36:44 +0900

    이름 자기소개 입니다.

---
+++ src/com/java_sample/selfIntroduction.java
@@ -0,0 +1,20 @@
+package com.java_sample;
+
+import java.util.Scanner;
+
+public class selfIntroduction {
+
+	// === 소개하기 프로그램입니다. ===
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("이름을 입력하세요 ");
+		System.out.println(" > ");
+		String name = scanner.next();
+		System.out.println();
+		System.out.println(name + "학생입니다. 자바프로그램 입문을 축하합니다.");
+		System.out.println("웰컴 투 헬 ~~~ (\\ .. /)/");		
+	}
+
+}
+
