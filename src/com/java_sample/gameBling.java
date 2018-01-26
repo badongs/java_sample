54c07e9e20cc5e529e6a55f883d50fcba16ef0a7
badongsch <badongsch@gmail.com>
2018-01-26 18:18:52 +0900

    수정

--- src/com/java_sample/gameBling.java
+++ src/com/java_sample/gameBling.java
@@ -1,7 +1,7 @@
 package com.java_sample;
 
 public class gameBling {
-
+	// 랜덤 달러를 가져와 원화로 환전 하기.
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i = (int)(Math.random()*1000)^6;
